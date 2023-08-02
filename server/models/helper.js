59916da9fbf947e7fd97c6c3323a962c49c09f51
Ramya Non-IU <ramyaram2092@outlook.com>
2023-08-02 12:19:59 -0700

    added factorial module that does f(n-1)+f(n-2)

---
+++ server/models/helper.js
@@ -0,0 +1,15 @@
+
+function fibonacci(n,prev,curr) {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+  
+  
+    const next = prev + curr;
+  
+    return next;
+  }
+  
+  module.exports=fibonacci;
+  
+  
+  
