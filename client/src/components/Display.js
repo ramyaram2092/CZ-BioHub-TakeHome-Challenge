387a305bedf2ac77adcf2012db0b57ff8480b1cb
Ramya Non-IU <ramyaram2092@outlook.com>
2023-08-02 16:03:48 -0700

    Rendering the fib sequence in second page

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Display = () => {
-    const navigate = useNavigate();
-
+  const navigate = useNavigate();
+  const location = useLocation();
+  const res = location.state;
+  // console.log(res);
   return (
     <div>
       <h1>Fibonacci Sequence</h1>
+      {res && (
+        <div>
+          <ul style={{ listStyle: "none", padding: 0, display: "flex" }}>
+            {res.map((value, index) => (
+              <li key={index} style={{ marginRight: "20px" }}>
+                {value},
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button
         className="btn btn-outline-secondary"
         type="button"
